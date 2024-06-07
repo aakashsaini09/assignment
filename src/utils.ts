728f30a3b88cd3476c0e5b82ee32d01332d72d7d
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-07 12:23:42 +0530

    adding schema

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgresql://first_owner:3PQDV6umNjst@ep-late-snow-a5o89d38.us-east-2.aws.neon.tech/first?sslmode=require");
     await client.connect();
     return client;
 }
