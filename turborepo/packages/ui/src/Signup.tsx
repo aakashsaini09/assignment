d83cde1b4313189d2dc203f1d4f142fd1e77d2b7
aakashsaini09 <aakashsaini948585@gmail.com>
2024-06-14 15:57:41 +0530

    changing card

--- turborepo/packages/ui/src/Signup.tsx
+++ turborepo/packages/ui/src/Signup.tsx
@@ -3,7 +3,7 @@ export const Signup = () => {
         <div style={{width: 400, border: "1px solid black"}}>
         <input type="text" placeholder="email" />
         <input type="password" placeholder="Enter password"/>
-        <button>Submit</button>
+        <button>Submit button</button>
         </div>
     </div>
 }
