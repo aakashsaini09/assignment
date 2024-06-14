44e021e4d1d1fba66623ee90208c56e0e8874bd8
Aakash Saini <121600651+aakashsaini09@users.noreply.github.com>
2024-06-14 22:06:35 +0530

    Update index.js

--- 12-3/common/dist/index.js
+++ 12-3/common/dist/index.js
@@ -1,9 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.signupInput = void 0;
-const zod_1 = require("zod");
-exports.signupInput = zod_1.z.object({
-    username: zod_1.z.string(),
-    password: zod_1.z.string()
-});
-console.log("hi there");
+// CloudFront
+{
+        "Version": "2008-10-17",
+        "Id": "PolicyForCloudFrontPrivateContent",
+        "Statement": [
+            {
+                "Sid": "AllowCloudFrontServicePrincipal",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "cloudfront.amazonaws.com"
+                },
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::webapplication-w19/*",
+                "Condition": {
+                    "StringEquals": {
+                      "AWS:SourceArn": "arn:aws:cloudfront::891376982051:distribution/E2MZTLAXT8HR8R"
+                    }
+                }
+            }
+        ]
+      }
