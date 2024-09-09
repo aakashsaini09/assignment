7171754abe55ff53588e96b580ebdc8dda79bbd3
aakashsaini09 <aakashsaini948585@gmail.com>
2024-09-09 15:32:20 +0530

    init

--- web-sockets/src/index.ts
+++ web-sockets/src/index.ts
@@ -10,10 +10,10 @@ const server = http.createServer(function(request: any, response: any){
 const wss = new WebSocketServer({server});
 wss.on('connection', function connection(ws){
     ws.on('error', console.error);
-    ws.on('message', function message(data, isBinary){
+    ws.on('message', function message(data){
         wss.clients.forEach(function each(client){
             if(client.readyState === WebSocket.OPEN){
-                client.send(data, {binary: isBinary});
+                client.send(data);
             }
         })
     });
