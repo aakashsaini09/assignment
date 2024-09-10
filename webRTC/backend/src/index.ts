29bce8fbfa4bfaea129840f9a843b29ec7fdbcd4
aakashsaini09 <aakashsaini948585@gmail.com>
2024-09-10 13:36:25 +0530

    RTC messages

--- webRTC/backend/src/index.ts
+++ webRTC/backend/src/index.ts
@@ -9,6 +9,15 @@ wss.on('connection', function connection(ws){
     ws.on('error', console.error);
     ws.on('message', function message(data: any){
         const message = JSON.parse(data);
+        if(message.type === 'identify-as-sender'){
+            senderSocket = ws;
+        }else if(message.type === 'identify-as-reciver'){
+            reciverSocket = ws;
+        }else if(message.type === 'create-offer'){
+            reciverSocket?.send(JSON.stringify({type: 'offer', offer: message.offer}))
+        }else if(message.type === 'create-answer'){
+            senderSocket?.send(JSON.stringify({type: 'offer', offer: message.offer}))
+        }
         // TAsk to complete
         // 1.) Create offer
         // 2.) Create Ans
