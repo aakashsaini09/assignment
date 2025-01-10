28e05488dfbb4bbd799a290ec4d4bb8386e5e06f
aakashsaini09 <aakashsaini948585@gmail.com>
2025-01-10 14:04:05 +0530

    1-easy

---
+++ jest-case/1-easy/src/tests/index.test.ts
@@ -0,0 +1,7 @@
+import { sum } from "../index";
+import { describe, it} from '@jest/globals'
+
+
+describe('sum', () => {
+    it('should be albe to add two numbers')
+})
--- jest-case/express-app-2/src/bin.ts
+++ jest-case/express-app-2/src/bin.ts
@@ -1,5 +1,14 @@
 import { app } from "./index";
-
+import { describe, expect, it} from '@jest/globals'
+import request from 'supertest'
+describe('POST /sum', async() => {
+    const res = await request(app).post('/sum').send({
+        a: 0,
+        b: 0
+    })
+    expect(res.statusCode).toBe(200)
+    expect(res.body.ans).toBe(0)
+})
 app.listen(3000, () => {
  
 });
