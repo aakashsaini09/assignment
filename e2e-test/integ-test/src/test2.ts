858ae6b1956b287a772dcbc7872051028b97caa6
aakashsaini09 <aakashsaini948585@gmail.com>
2025-05-08 14:56:42 +0530

    test case

--- e2e-test/integ-test/src/test2.ts
+++ e2e-test/integ-test/src/test2.ts
@@ -33,44 +33,44 @@ import { app } from ".";
 // mockReturnValue
 vi.mock("../db");
 
-describe("Tests the sum function", () => {
-    it("Should return 3 when 1 + 2", async () => {
-        prismaClient.request.create.mockResolvedValue({
-            id: 1,
-            answer: 3,
-            type: "Sum",
-            a: 1,
-            b: 2
-        })
+// describe("Tests the sum function", () => {
+//     it("Should return 3 when 1 + 2", async () => {
+//         prismaClient.request.create.mockResolvedValue({
+//             id: 1,
+//             answer: 3,
+//             type: "Sum",
+//             a: 1,
+//             b: 2
+//         })
 
-        vi.spyOn(prismaClient.request, "create");
+//         vi.spyOn(prismaClient.request, "create");
 
-        const res = await request(app).post("/sum").send({
-            a: 1,
-            b: 2
-        })
+//         const res = await request(app).post("/sum").send({
+//             a: 1,
+//             b: 2
+//         })
 
-        expect(prismaClient.request.create).toHaveBeenCalledWith({
-            data: {
-                a: 1,
-                b: 2,
-                type: "Sum",
-                answer: 3
-            }
-        })
+//         expect(prismaClient.request.create).toHaveBeenCalledWith({
+//             data: {
+//                 a: 1,
+//                 b: 2,
+//                 type: "Sum",
+//                 answer: 3
+//             }
+//         })
 
-        expect(res.body.answer).toBe(3);
-        expect(res.body.id).toBe(1);
-        expect(res.statusCode).toBe(200);
-    })
+//         expect(res.body.answer).toBe(3);
+//         expect(res.body.id).toBe(1);
+//         expect(res.statusCode).toBe(200);
+//     })
 
-    it("Should fail when a number is too big", async () => {
-        const res = await request(app).post("/sum").send({
-            a: 1000000000000,
-            b: 2
-        })
+//     it("Should fail when a number is too big", async () => {
+//         const res = await request(app).post("/sum").send({
+//             a: 1000000000000,
+//             b: 2
+//         })
 
-        expect(res.body.message).toBe("Sorry we dont support big numbers");
-        expect(res.statusCode).toBe(422);
-    })
-})
+//         expect(res.body.message).toBe("Sorry we dont support big numbers");
+//         expect(res.statusCode).toBe(422);
+//     })
+// })
